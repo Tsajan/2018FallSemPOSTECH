e09a01c4537d2268532c403043ea7bac919a7631
Sajan Maharjan <sajan.maharjan294@gmail.com>
2018-09-13 09:08:49 +0900

    Code for assignment 2 updated

--- CSED490U Blockchain & Cryptocurrency/Princeton BitCoin & CryptoCurrency/assignment2starterCode/assignment2starterCode/CompliantNode.java
+++ CSED490U Blockchain & Cryptocurrency/Princeton BitCoin & CryptoCurrency/assignment2starterCode/assignment2starterCode/CompliantNode.java
@@ -1,26 +1,40 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
-
+	private double p_graph;
+	private double p_malicious;
+	private double p_txDistribution;
+	private int numRounds;
+	private boolean[] followees;
+	private Set<Transaction> pendingTransactions;
+	
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+        this.p_graph = p_graph;
+        this.p_malicious = p_malicious;
+        this.p_txDistribution = p_txDistribution;
+        this.numRounds = numRounds;
+        this.pendingTransactions = new HashSet<Transaction>();
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        this.followees = followees;
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        for(Transaction tx: pendingTransactions)
+    	this.pendingTransactions.add(tx);
     }
 
     public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
+        return this.pendingTransactions;
+        
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+    	for(Candidate c: candidates)
+    		this.pendingTransactions.add(c.tx);
     }
 }
