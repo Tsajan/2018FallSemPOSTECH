8153c3a32c7cb0fcd879524f1438ae2f504498cc
Sajan Maharjan <sajan.maharjan294@gmail.com>
2018-12-04 17:37:22 +0900

    BMS files updated

--- CSED490U Blockchain & Cryptocurrency/BMS/app.js
+++ CSED490U Blockchain & Cryptocurrency/BMS/app.js
@@ -21,15 +21,6 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 app.get('/bms', function (req, res) {
-	var eth_block_heights = [];
-	var eth_block_ts = [];
-	var eth_block_tnums = [];
-	var eth_block_miners = [];
-	var eth_block_sizes = [];
-
-	var eth_txn_txnhash = [];
-	var eth_txn_age = [];
-	var eth_txn_amt = [];
 
 	var ethTransJSON;
 	var ethBlockJSON;
@@ -63,44 +54,68 @@ app.get('/bms', function (req, res) {
         });
 
     });
-	/*
-	con.connect(function(err) {
-  		if (err) throw err;
-  		console.log("Connected to MySQL!");
-
-  		var eth_block_sql = "select blocknumber, ts, 12 as tnum, miner, size from ethBlock order by blockNumber desc limit 3";
-  		con.query(eth_block_sql, function(err, result, fields) {
-			if (err) throw err;
-
-			for(i=0; i < result.length; i++) {
-				eth_block_heights.push(result[i]['blocknumber'])
-				eth_block_ts.push(result[i]['ts'])
-				eth_block_tnums.push(result[i]['tnum'])
-				eth_block_miners.push(result[i]['miner'])
-				eth_block_sizes.push(result[i]['size'])
-			}
-			res.render('index', { eth_block_heights: eth_block_heights, eth_block_ts: eth_block_ts, eth_block_tnums: eth_block_tnums, eth_block_miners: eth_block_miners, eth_block_sizes: eth_block_sizes });
-		});
-		
-  		var eth_txn_sql = "select hash as txnhash, '10 minutes ago' as age, ethValue as amt from ethtransaction order by hash desc limit 3";
-		con.query(eth_txn_sql, function(err, result, fields) {
-			if (err) throw err;
-			for(i=0; i < result.length; i++) {
-				eth_txn_txnhash.push(result[i]['txnhash'])
-				eth_txn_age.push(result[i]['age'])
-				eth_txn_amt.push(result[i]['amt'])
-			}
-			res.render('index', {eth_txn_txnhash: eth_txn_txnhash, eth_txn_age: eth_txn_age, eth_txn_amt: eth_txn_amt});
-		})
-		
-
-	});
-	*/
-	
 })
+
+app.post('/search', function(req, res) {
+
+	var ethblockinfo;
+
+	console.log("Search started");
+	var hashaddrflg = 0;
+	var inputField = req.body.searchval;
+	console.log("User search query: " + inputField);
+	if(inputField.startsWith('0x')) {
+		console.log("Search for a hash value entered by user");
+		hashaddrflg = 1;
+	}
+  		/*
+  		if(hashaddrflg) {
+  			inputField = inputField.substring(2);
+  			console.log("Search hash value": + inputField);
+  			var sql = 'select count(*) from ethblock where blocknumber = ?';
+
+  			connection.query(sql, [inputField], function(err, result, fields) {
+  				if(err) throw err;
+  				console.log(result);
+  			});
+  		}
+  		*/
+  	if(!hashaddrflg) {
+ 		var blocksql = "select count(*) as cnt from ethblock where blocknumber = ?";
+  		connection.query(blocksql, [inputField], function(err, result, fields) {
+  			if(err) throw err;
+  			console.log(result);
+  			if(result[0].cnt) {
+  				console.log("Matching record found");
+  				var nextblocksql = "select blocknumber, miner, date_format(ts,'%Y-%m-%d %H:%i:%s') as ts, difficulty, size, nonce, sha3uncles,  ifnull(transactions,0) as numtxn, ifnull(uncles,0) as numuncles, totaldifficulty, gasused, gaslimit, hash, parenthash from ethblock where blocknumber = ?";
+  				connection.query(nextblocksql, [inputField], function(err, out, fields) {
+  					if(err) throw err;
+  					ethblockinfo = out;
+  					console.log(out);
+  					res.render('block', { ethblockinfo: ethblockinfo });
+  				})
+  			}
+  			else {
+  				console.log("No records found");
+  				res.write("No records found");
+  			}
+  		});
+  	}
+  	else {
+  		inputField = inputField.substr(2);
+  		console.log(inputField);
+  		var sql = "select count(1) as cnt from ethblock where hash = ? union select count(1) from ethtransaction where hash = ?";
+  		connection.query(sql, [inputField, inputField], function(err, result, fields) {
+  			if(err) throw err;
+  			console.log(result);
+  		});
+  	}
+  	
+});
 
 app.get('/eth/block', function(req, res) {
 	console.log("Rendering block information page");
+	//var inputBlockNum = req.body.
 	res.render('block');
 })
 
